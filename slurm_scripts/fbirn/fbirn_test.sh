e7784188e60cfcf782558e0c714e66650b111c65
pnpm-aftab <maftab1@student.gsu.edu>
2026-05-30 00:01:18 -0400

    more prefetches

--- slurm_scripts/fbirn/fbirn_test.sh
+++ slurm_scripts/fbirn/fbirn_test.sh
@@ -39,8 +39,8 @@ python3 train_script_fixed_seed.py \
     experiment.cv_folds=2 \
     experiment.epochs=3 \
     experiment.numvolumes=3 \
-    experiment.num_workers=8 \
-    experiment.prefetches=4 \
+    experiment.num_workers=4 \
+    experiment.prefetches=8 \
     model.masked=False \
     model.model_channels=64
 
